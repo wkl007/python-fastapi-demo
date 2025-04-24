628ce8ad5c978ce4fc6a4f818fbdb77950e19917
wkl007 <499657357@qq.com>
2025-04-24 18:32:34 +0800

    feat: 基础 demo

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from python-fastapi-demo!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None
+
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: str | None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id, "item_price": item.price}
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=9000)
