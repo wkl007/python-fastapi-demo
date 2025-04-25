d2283f4b8dea4191a68870b14a30f02eaf91eacb
wkl007 <499657357@qq.com>
2025-04-25 16:12:32 +0800

    feat: CORS 配置

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 
 import jwt
 from fastapi import Depends, FastAPI, HTTPException, status, Request
@@ -26,6 +27,21 @@ fake_users_db = {
 }
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware("http")
