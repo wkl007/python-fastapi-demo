39c2dfb15c0ba21c9e01a5386c8e3a70207904bb
wkl007 <499657357@qq.com>
2025-04-25 14:53:58 +0800

    feat: 处理错误

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from enum import Enum
 from typing import Annotated, Literal, Any
 from uuid import UUID
 
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, status, Form, UploadFile, File
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, status, Form, UploadFile, File, HTTPException, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, PlainTextResponse
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 app = FastAPI()
 
@@ -345,5 +350,64 @@ async def create_file(file: Annotated[bytes, File()], fileb: Annotated[UploadFil
         'fileb_content_type': fileb.content_type
     }
 
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get('/items6/{item.id}')
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail='Item not found', headers={'X-ERROR': 'There goes my error'})
+    return {'item': items[item_id]}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."}
+    )
+
+
+@app.get('/unicorn/{name}')
+async def read_unicorn(name: str):
+    if name == 'yolo':
+        raise UnicornException(name=name)
+    return {'unicorn_name': name}
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+
+@app.get("/items7/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
+
+
+class Item(BaseModel):
+    title: str
+    size: int
+
+
+@app.post("/items7/")
+async def create_item(item: Item):
+    return item
 # if __name__ == '__main__':
 #     uvicorn.run(app, host='0.0.0.0', port=9000)
