8c28d36d2c41494c24e4d05dd02fdd6c5afdb74f
wkl007 <499657357@qq.com>
2025-04-25 15:18:44 +0800

    feat: 类作为依赖项

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from enum import Enum
 from typing import Annotated, Literal, Any
 from uuid import UUID
 
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, status, Form, UploadFile, File, HTTPException, Request
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, status, Form, UploadFile, File, HTTPException, Request, \
+    Depends
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse, PlainTextResponse
@@ -434,5 +435,40 @@ def update_item(id: str, item: Item):
     json_compatiable_item_data = jsonable_encoder(item)
     fake_db[id] = json_compatiable_item_data
     return json_compatiable_item_data
+
+
+async def common_parameters(
+        q: str | None = None, skip: int = 0, limit: int = 100
+):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items8/", tags=['items'])
+async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get('/items9', tags=['items'])
+async def read_items(commons: Annotated[CommonQueryParams, Depends()]):
+    response = {}
+    if commons.q:
+        response.update({'q': commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+
 # if __name__ == '__main__':
 #     uvicorn.run(app, host='0.0.0.0', port=9000)
